34246e75894cd9d83caab65a7b3dccc84285accb
quangminh15 <phamm7696@gmail.com>
2023-05-23 17:19:41 +0700

    them du an moi

---
+++ test-git-2/src/main/java/com/fpoly/ShopBanGiay/TestGit2Application.java
@@ -0,0 +1,13 @@
+package com.fpoly.ShopBanGiay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestGit2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestGit2Application.class, args);
+	}
+
+}
---
+++ test-git-2/src/test/java/com/fpoly/ShopBanGiay/TestGit2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fpoly.ShopBanGiay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestGit2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
