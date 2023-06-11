cef1e342ca6c342bebb3a3ace349d2793dd4f1b8
HAI <volonghai1511@gmail.com>
2023-06-11 19:29:35 +0700

    [ECLPSE]_GioHang_Cập Nhật Số Lượng

--- Nhom4_ShopBanGiay_ASM/src/main/java/com/fpoly/ShopBanGiay/dao/SanPhamSizeDAO.java
+++ Nhom4_ShopBanGiay_ASM/src/main/java/com/fpoly/ShopBanGiay/dao/SanPhamSizeDAO.java
@@ -16,4 +16,7 @@ public interface SanPhamSizeDAO extends JpaRepository<SanPhamSize, Integer>{
 	
 	@Query("select z from SanPhamSize z where z.chitietdonhang.donhang.madh like ?1")
 	List<SanPhamSize> findByMaDH(Integer madh);
+//	
+//	@Query ("select sum(z.giohang.soluong*z.sanpham.gia) from SanPhamSize z where z.giohang.nguoidung.mand like ?1")
+//	 Integer tongTien(Integer mand);
 }
