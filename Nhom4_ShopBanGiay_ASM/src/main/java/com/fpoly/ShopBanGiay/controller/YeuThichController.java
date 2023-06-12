fd95e8b639964785f2f6cbd3c171a032a3f80e60
Tieen001 <Minhtien13052003@gmail.com>
2023-06-12 21:43:49 +0700

    [ECLIPSE] edit Yeuthich Sua duong dan bi sai

--- Nhom4_ShopBanGiay_ASM/src/main/java/com/fpoly/ShopBanGiay/controller/YeuThichController.java
+++ Nhom4_ShopBanGiay_ASM/src/main/java/com/fpoly/ShopBanGiay/controller/YeuThichController.java
@@ -2,6 +2,7 @@ package com.fpoly.ShopBanGiay.controller;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
@@ -11,10 +12,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.fpoly.ShopBanGiay.dao.NguoiDungDAO;
+import com.fpoly.ShopBanGiay.dao.SanPhamDAO;
+import com.fpoly.ShopBanGiay.dao.SanPhamSizeDAO;
 import com.fpoly.ShopBanGiay.dao.YeuThichDAO;
 import com.fpoly.ShopBanGiay.model.NguoiDung;
+import com.fpoly.ShopBanGiay.model.SanPham;
+import com.fpoly.ShopBanGiay.model.SanPhamSize;
 import com.fpoly.ShopBanGiay.model.YeuThich;
 import com.fpoly.ShopBanGiay.service.SessionService;
 import com.fpoly.ShopBanGiay.service.YtServiceImp;
@@ -33,26 +39,63 @@ public class YeuThichController {
 	@Autowired
 	YtServiceImp ytimp;
 	
+	@Autowired
+	SanPhamDAO sanphamDAO;
+	
+	@Autowired
+	SanPhamSizeDAO spsizeDAO;
+	
+	
 	@GetMapping("/yeuthich")
 	public String DangNhap(Model model, YeuThich yt) {
-		List<YeuThich> yeu = yeuthichDao.findYeuThichByID(2);
+		NguoiDung nguoidung = new NguoiDung(sessionService.getSessionAttribute("user"));
+//		System.out.println("u:" +nguoidung);
+		List<YeuThich> yeu = yeuthichDao.findYeuThichByID(nguoidung.getMand());
 		model.addAttribute("yeus", yeu);
 		return "/nguoidung/yeuthich";
 	}
 	
 	@RequestMapping("/addyeuthich/{masp}")
-	public String add1(@PathVariable("masp")Integer masp) {
-		NguoiDung nguoidung = nguoidungDao.findById(2).get();
-		Date da = ytimp.add(nguoidung, null, masp);
+	public String add1(@PathVariable("masp")Integer masp, Model model) {
+		Optional<YeuThich> y = yeuthichDao.findById(masp);
+		NguoiDung nguoidung1 = new NguoiDung(sessionService.getSessionAttribute("user"));
+		if(y.isPresent()) {
+			NguoiDung nguoidung = nguoidungDao.findById(nguoidung1.getMand()).get();
+			Date da = ytimp.add(nguoidung, null, masp);
+		}else {
+			NguoiDung nguoidung = nguoidungDao.findById(nguoidung1.getMand()).get();
+			Date da = ytimp.add(nguoidung, null, masp);
+		}
+		
 		return "redirect:/yeuthich";
 	}
 	
 	@RequestMapping("/delete_yeuthich/{mayeuthich}")
 	public String delete_yeuthich(Model model, @PathVariable(name = "mayeuthich") Integer mayeuthich) {
+		NguoiDung nguoidung = new NguoiDung(sessionService.getSessionAttribute("user"));
+//		System.out.println("u:" +nguoidung);
+		List<YeuThich> yeu = yeuthichDao.findYeuThichByID(nguoidung.getMand());
 		yeuthichDao.deleteById(mayeuthich);
-		List<YeuThich> yeu = yeuthichDao.findYeuThichByID(2);
 		model.addAttribute("yeus", yeu);
 		return "redirect:/yeuthich";
+	}
+	
+	@RequestMapping("/sanpham1/chitietsp/{masp}")
+	public String chitietspa(Model model, @PathVariable("masp") Integer masp, @Param("masps") Integer masps,
+			@RequestParam("keywords") Optional<String> kw) {
+		SanPham sanpham = sanphamDAO.findById(masp).orElse(null);
+		model.addAttribute("sanpham", sanpham);
+		
+		String kwords = kw.orElse(sanpham.getDanhmuc().getTendm());
+		sessionService.getSessionAttribute("keywords");
+		sessionService.setSessionAttribute("keywords", kwords);
+		List<SanPham> sanphams = sanphamDAO.findByTenDM("%" + kwords + "%");
+		model.addAttribute("sanphams", sanphams);
+
+		List<SanPhamSize> spsizes = spsizeDAO.findByMaSP(masp);
+		model.addAttribute("sizes", spsizes);
+
+		return "/nguoidung/chitietsp";
 	}
 	
 }
